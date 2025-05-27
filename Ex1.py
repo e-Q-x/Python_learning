21148ddce058ddb561cdadd8bc951f6485b74ba6
e-Q-x <9038348@gmail.com>
2025-05-27 20:13:09 +0300

    les3

--- Ex1.py
+++ Ex1.py
@@ -2,16 +2,16 @@
 # отчество, возраст, номер телефона. Добавить эти данные в словарь.
 # Вывести весь словарь на экран. Вывести на экран только имя и фамилию
 name = input("Введите имя: ")
-surname = input("Введите свою фамилию:")
-surname2 = input("Введите свое отчество:")
-age = int(input("Введите свой возраст:"))
-phone = int(input("Введите свой номер телефона:"))
+surname = input("Введите фамилию:")
+surname2 = input("Введите отчество:")
+age = int(input("Введите возраст:"))
+phone = int(input("Введите номер телефона:"))
 person = {}
-person[name] = name
-person[surname] = surname
-person[surname2] = surname2
-person[age] = age
-person[phone] = phone
+person["name"] = name
+person["surname"] = surname
+person["surname2"] = surname
+person["age"] = age
+person["phone"] = phone
 print(person)
-print(person[name], [surname])
+print(person["name"], person["surname"])
 
